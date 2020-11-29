f0b718c977119b6982fe03f02306f3ee9e91651f
Jack Case <jackacase@gmail.com>
2020-11-29 03:56:33 +0000

    get rid of autogenerated pages

---
+++ src/views/Landing.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Two Rooms and a Boom</h1>
+    <button>Create Game</button>
+    <button>Join Game</button>
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+  components: {
+
+  }
+}
+</script>
